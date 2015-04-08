80f50b489c063fd8158389a5871e7e460994b09a
Uku Pattak <uku.pattak@transferwise.com>
2015-04-08 08:21:22 +0300

    Initial commit

---
+++ tasks/inject_dependencies.js
@@ -0,0 +1,22 @@
+/*
+ * grunt-inject-dependencies
+ * https://github.com/ukupat/grunt-inject-dependencies
+ *
+ * Copyright (c) 2015 Uku Pattak
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('inject_dependencies', 'Create grunt-include-source tags to HTML', function() {
+    for (var app in this.data) {
+      if (this.data.hasOwnProperty(app)) {
+        grunt.file.write(
+          this.data[app].dist,
+          grunt.file.read(this.data[app].src) + '\n<!-- include: "type": "js", "files": ["' + this.data[app].dependencies.join('", "') + '"] -->\n<!-- /include -->'
+        );
+      }
+    }
+  });
+};
